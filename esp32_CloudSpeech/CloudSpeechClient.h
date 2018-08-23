462dc2b60a23a119c4172beadcee8c9cae87d245
Mhage <masahage@soleil.ocn.ne.jp>
2018-08-23 22:36:39 +0900

    Add files via upload

---
+++ esp32_CloudSpeech/CloudSpeechClient.h
@@ -0,0 +1,18 @@
+#ifndef _CLOUDSPEECHCLIENT_H
+#define _CLOUDSPEECHCLIENT_H
+#include <WiFiClientSecure.h>
+#include "Audio.h"
+
+class CloudSpeechClient {
+  WiFiClientSecure client;
+  void PrintHttpBody2(Audio* audio);
+  bool useApiKey;
+
+public:
+  CloudSpeechClient(bool useApiKey = false);
+  ~CloudSpeechClient();
+  void Transcribe(Audio* audio);
+};
+
+#endif // _CLOUDSPEECHCLIENT_H
+
---
+++ esp32_CloudSpeech/esp32_CloudSpeech.ino
@@ -0,0 +1,17 @@
+#include "Audio.h"
+#include "CloudSpeechClient.h"
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("\r\nRecord start!\r\n");
+  Audio* audio = new Audio();
+  audio->Record();
+  CloudSpeechClient* cloudSpeechClient = new CloudSpeechClient();
+  cloudSpeechClient->Transcribe(audio);
+  delete cloudSpeechClient;
+  delete audio;
+}
+
+void loop() {
+}
